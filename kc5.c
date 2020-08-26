532da7d0dbdc57da980783833724ba6e8b4aa5ef
sagoj0_ <nisi3genki@gmail.com>
2020-08-26 21:57:40 +0900

    implement add/sub

--- kc5.c
+++ kc5.c
@@ -7,11 +7,30 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
+    char *p = argv[1];
+
     puts(".intel_syntax noprefix");
     puts(".globl main");
     puts("main:");
-    printf("  mov rax, %d\n", atoi(argv[1]));
-    puts("  ret");
+    printf("  mov rax, %ld\n", strtol(p, &p, 10));
 
+    while(*p) {
+        if (*p == '+') {
+            p++;
+            printf("  add rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        if (*p == '-') {
+            p++;
+            printf("  sub rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        fprintf(stderr, "unexpected character: '%c'\n", *p);
+        return 1;
+    }
+
+    puts("  ret");
     return 0;
 }
